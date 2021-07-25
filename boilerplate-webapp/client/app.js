3063a103026249119c2da235d8f27930445027d7
pdl39 <dhlee9111@gmail.com>
2021-07-24 20:07:39 -0400

    ADD: app.js

---
+++ boilerplate-webapp/client/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <main>Webapp Boilerplate!</main>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
