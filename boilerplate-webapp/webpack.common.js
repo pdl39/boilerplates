756248a526b82754519f4f94aa9aa7e23c6117be
pdl39 <dhlee9111@gmail.com>
2021-07-24 20:10:12 -0400

    ADD: Webpack config setup

---
+++ boilerplate-webapp/webpack.common.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'client', 'app')
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.js', '.json'],
+    mainFiles: ['index'],
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-react',
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'dist/src/index.html')
+    })
+  ]
+}
---
+++ boilerplate-webapp/webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist/assets'),
+    historyApiFallback: true,
+    hot: true
+  }
+});
