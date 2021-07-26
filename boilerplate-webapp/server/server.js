a012c5d0f4b220e0e97c0d4b67c29b3e815274a4
pdl39 <dhlee9111@gmail.com>
2021-07-26 14:01:29 -0400

    ADD: express server app with middlewares

---
+++ boilerplate-webapp/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+
+// CREATE SERVER APP
+const app = express();
+
+// SET UP MIDDLEWARES
+// logging middleware
+app.use(morgan('dev'));
+
+// static middleware
+const staticAssetsPath = path.resolve(__dirname, '..', 'dist');
+app.use(express.static(staticAssetsPath));
+
+// parsing middlewares
+app.use(express.json());
+app.use(express.urlencoded());
+
+// API routes
+app.use('/api', require('./api/apiRoutes'));
