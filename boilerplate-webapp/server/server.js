60a3a9bfe942cb0186393b05ad04440077d7573a
pdl39 <dhlee9111@gmail.com>
2021-07-26 15:39:47 -0400

    UPDATE: error handler

--- boilerplate-webapp/server/server.js
+++ boilerplate-webapp/server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 // CREATE SERVER APP
 const app = express();
 
-// SET UP MIDDLEWARES
+// MIDDLEWARES
 // logging middleware
 app.use(morgan('dev'));
 
@@ -18,11 +18,22 @@ app.use(express.static(staticAssetsPath));
 app.use(express.json());
 app.use(express.urlencoded());
 
-// API routes
+
+// API ROUTES
 app.use('/api', require('./api/apiRoutes'));
 
+
+// FALLBACK HANDLER
 // send index.html as fallback
 app.get('*', (req, res) => {
   const indexHtmlPath = path.resolve(__dirname, '..', 'dist', 'index.html');
   res.sendFile(indexHtmlPath);
+});
+
+
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+  console.log(err);
+  console.log(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
