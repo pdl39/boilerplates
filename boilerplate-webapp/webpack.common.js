d7d4d6620f6dfb54dbaf2afc905c36dd208b3846
pdl39 <dhlee9111@gmail.com>
2021-07-30 06:36:50 -0400

    REMOVE: @babel/preset-env

--- boilerplate-webapp/webpack.common.js
+++ boilerplate-webapp/webpack.common.js
@@ -21,8 +21,7 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               presets: [
-                '@babel/preset-react',
-                '@babel/preset-env'
+                '@babel/preset-react'
               ]
             }
           }
