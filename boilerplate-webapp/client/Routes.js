48ccdf9d2df0f343da09cae03be3b8fa4470a0dc
pdl39 <dhlee9111@gmail.com>
2021-07-30 06:41:03 -0400

    UPDATE: include routes for login & signup

--- boilerplate-webapp/client/Routes.js
+++ boilerplate-webapp/client/Routes.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from "./components/Home.js";
+import Login from './components/Login.js';
+import Signup from "./components/Signup.js";
 
 const Routes = (props) => {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route eact path="/signup">
+        <Signup />
       </Route>
     </Switch>
   )
